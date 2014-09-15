a0f9306892cde26cf9fae5adc4df43f2eb5ecae2
Benjamin Dumke-von der Ehe <mail@benjamin-dumke.de>
2014-09-15 14:04:30 +0200

    improve terminology

--- messenger.js
+++ messenger.js
@@ -4,58 +4,58 @@ function Messenger(localStorageKey, receiveOwn) {
     var listeners = [];
     var pendingRequests = {};
     
-    function makeMessage(data) {
+    function makeEnvelope(message) {
         var result = {
             sender: myId,
             time: Date.now(),
-            count: counter,
-            data: data
+            id: counter,
+            message: message
         };        
         counter++;
         return result;
     }
     
-    function send(message) {
-        var messageString = JSON.stringify(message);
+    function send(envelope) {
+        var serialized = JSON.stringify(envelope);
         try {
-            localStorage.setItem(localStorageKey, messageString);
+            localStorage.setItem(localStorageKey, serialized);
         } catch (ex) {}
         if (receiveOwn)
-            setTimeout(function () { notifyListeners(message); }, 0);
+            setTimeout(function () { notifyListeners(envelope); }, 0);
     }
         
-    function broadcast(data) {
-        send(makeMessage(data));
+    function broadcast(message) {
+        send(makeEnvelope(message));
     }
     
-    function unicast(data, recipient) {
-        var message = makeMessage(data);
-        message.recipient = recipient;
-        send(message);
+    function unicast(message, recipient) {
+        var envelope = makeEnvelope(message);
+        envelope.recipient = recipient;
+        send(envelope);
     }
     
-    function request(data, callback, timeoutMs, recipient) {
-        var message = makeMessage(data);
-        message.rsvp = true;
-        pendingRequests[message.count] = [];
+    function request(message, callback, timeoutMs, recipient) {
+        var envelope = makeEnvelope(message);
+        envelope.rsvp = true;
+        pendingRequests[envelope.id] = [];
         setTimeout(function () {
-            var responses = pendingRequests[message.count];
-            delete pendingRequests[message.count];
-            var data = [];
+            var responses = pendingRequests[envelope.id];
+            delete pendingRequests[envelope.id];
+            var messages = [];
             for (var i = 0; i < responses.length; i++) {
-                data.push(responses[i].data);
+                messages.push(responses[i].message);
             }
-            callback(data, responses);
+            callback(messages, responses);
         }, timeoutMs || 50);
-        send(message);
+        send(envelope);
     }
     
-    function responderFor(message) {
-        return function (data) {
-            var response = makeMessage(data);
-            response.recipient = message.sender;
-            response.inResponseTo = message.count;
-            send(response);
+    function responderFor(envelope) {
+        return function (responseMsg) {
+            var responseEnv = makeEnvelope(responseMsg);
+            responseEnv.recipient = envelope.sender;
+            responseEnv.inResponseTo = envelope.id;
+            send(responseEnv);
         }
     }
     
@@ -65,35 +65,35 @@ function Messenger(localStorageKey, receiveOwn) {
         listeners.push(callback);
     }
 
-    function notifyListeners(message) {
-        if ("recipient" in message) {
-            if (message.recipient !== myId)
+    function notifyListeners(envelope) {
+        if ("recipient" in envelope) {
+            if (envelope.recipient !== myId)
                 return;
-            if ("inResponseTo" in message) {
-                var pr = pendingRequests[message.inResponseTo];
+            if ("inResponseTo" in envelope) {
+                var pr = pendingRequests[envelope.inResponseTo];
                 if (!pr)
                     return; // reponse came after timeout -- too late;
-                pr.push(message);
+                pr.push(envelope);
                 return;
             }
         }
         for (var i = 0; i < listeners.length; i++) {
-            if (message.rsvp)
-                listeners[i](message.data, message, responderFor(message));
+            if (envelope.rsvp)
+                listeners[i](envelope.message, envelope, responderFor(envelope));
             else
-                listeners[i](message.data, message);
+                listeners[i](envelope.message, envelope);
         }
     }
     
     function onStorage(evt) {
         if (evt.key !== localStorageKey)
             return;
-        var message = JSON.parse(evt.newValue);
+        var envelope = JSON.parse(evt.newValue);
 
-        if (message.sender === myId)
+        if (envelope.sender === myId)
             return;
         
-        notifyListeners(message);
+        notifyListeners(envelope);
     }
     
     return {
